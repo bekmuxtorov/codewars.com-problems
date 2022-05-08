71757cff921bab6f109450068a5760e86e997040
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-05-08 18:56:05 +0500

    add_binary

---
+++ add_binary.py
@@ -0,0 +1,11 @@
+#codewars.com 
+#problem 1
+#bekmuxtorov
+
+def add_binary(a,b):
+    bin_value = bin(a+b)
+    return bin_value[2:]
+
+print(f"{add_binary(1,1)}")
+
+#<========================================> 😎Bajarildi <========================================>
